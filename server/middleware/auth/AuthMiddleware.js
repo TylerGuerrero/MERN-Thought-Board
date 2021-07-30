3765a81745280a5b68122932634c8aa95644ce19
Tyler <guerrero.tyler3@gmail.com>
2021-07-30 14:42:35 -0400

    Updated auth routes

--- server/middleware/auth/AuthMiddleware.js
+++ server/middleware/auth/AuthMiddleware.js
@@ -1,25 +1,30 @@
 import jwt from 'jsonwebtoken'
 
+import User from '../../models/user/UserModel.js'
+
 const { decode, verify } = jwt
 
 export const authCheck = async (req, res, next) => {
-    const token = req.headers.authorization.spilt(" ")[1]
+    const token = req.headers.authorization.split(" ")[1]
     const isCustomAuth = token.length < 500
+    let decodedToken
 
     if (!token) return res.status(401).json({ error: "Token does not exist" })
-    let decodedToken;
 
     try {
         if (token && isCustomAuth) {
             decodedToken = verify(token, process.env.JWT_SECRET)
+            const user = await User.findById(decodedToken.id)
+            req.user = user
             req.userId = decodedToken?.id
         } else {
             decodedToken = decode(token)
             req.userId = decodedToken?.sub
-        }
+        }   
 
         next()
-    } catch (error) {
+    } catch(error) {
+        console.log(error)
         return res.status(401).json({ error: error.message })
     }
 }
--- server/routes/auth/AuthRoutes.js
+++ server/routes/auth/AuthRoutes.js
@@ -1,30 +1,32 @@
 import express from 'express'
 
-import User from '../../models/user/UserModel.js'
 import { authCheck } from '../../middleware/auth/AuthMiddleware.js'
 import { registrationValidation, loginValidation } from '../../validation/auth/AuthValidation.js'
+
+import User from '../../models/user/UserModel.js'
 
 const { Router } = express
 
 const router = Router()
 
 router.post("/register", async (req, res) => {
-    const { email, password, confirmPassword, firstName, lastName } = req.body
+    const { firstName, lastName, email, password, confirmPassword } = req.body
     const { error } = registrationValidation(req.body)
 
     if (error) return res.status(401).json({ error: error.details[0].message })
-    if (password !== confirmPassword) return res.status(401).json({ error: "Passwords do not match" })
-    
+    if (confirmPassword !== password) return res.status(401).json({ error: "Passwords do not match" })
+
     try {
-        let user = await User.findOne({ email }).exec()
+        let user = await User.findOne({ email })
 
         if (user) return res.status(401).json({ error: "User exist" })
 
         user = await User.create({ name: `${firstName} ${lastName}`, email, password })
         const token = user.getJWT()
-        res.cookie("jwt", token, { httpOnly: true, maxAge: 3*24*60*60})
+        res.cookie("jwt", token, { httpOnly: true, maxAge: 3*24*60*60 })
         return res.status(201).json({ result: user, token })
-    } catch (error) {
+    } catch(error) {
+        console.log(error)
         return res.status(500).json({ error: error.message })
     }
 })
@@ -40,14 +42,15 @@ router.post("/login", async (req, res) => {
         const token = user.getJWT()
         res.cookie("jwt", token, { httpOnly: true, maxAge: 3*24*60*60 })
         return res.status(201).json({ result: user, token })
-    } catch (error) {
+    } catch(error) {
+        console.log(error)
         return res.status(500).json({ error: error.message })
     }
 })
 
 router.get("/logout", authCheck, (req, res) => {
-    res.cookie("jwt", "", {httpOnly: true, maxAge: 3})
-    return res.status(201).json({ msg: "User Logged Out"})
+    res.cookie("jwt", " ", { httpOnly: true, maxAge: 3})
+    return res.status(201).json({ msg: "User logged out" })
 })
 
 export default router
