94908b2085976c03e452985e78ddbfde91b55f20
Tyler <guerrero.tyler3@gmail.com>
2021-08-06 15:52:17 -0400

    Added Navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { AppBar, Toolbar, Button, Typography, Avatar } from '@material-ui/core'
 import { useHistory, useLocation } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
@@ -17,18 +17,18 @@ import useStyles from './Styles'
 const Navbar = () => {
     const classes = useStyles()
 
-    const location = useLocation()
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')))
+
     const history = useHistory()
+    const location = useLocation()
 
     const dispatch = useDispatch()
 
-    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')))
-
     const logout = useCallback(() => {
-        dispatch(logoutAction())
+        dispatch(logoutAction)
         history.push("/")
         setUser(null)
-    }, [history, dispatch])
+    }, [dispatch, history])
 
     useEffect(() => {
         const token = user?.token
@@ -41,38 +41,35 @@ const Navbar = () => {
             }
         }
 
-        setUser(JSON.parse(localStorage.getItem('profile')))
-    }, [user?.token, location, logout])
+    }, [logout, user?.token, location])
 
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
             <div className={classes.brandContainer}>
-               <img className={classes.image} src={memoriesLogo} height="45px" title="Logo" alt="Logo" />
-               <img className={classes.image} src={memoriesText} height="40px" title="Memories" alt="memories" /> 
+                <img className={classes.image} src={memoriesLogo} alt="Memories Logo" title="Memories Logo" height="45px" />
+                <img className={classes.image} src={memoriesText} alt="Memories Text" title="Memories Text" height="40px" />
             </div>
             <Toolbar className={classes.toolbar}>
-                { 
-                    user ? (
-                        <div className={classes.profile}>
-                            <Avatar src={user.result.imageUrl} variant="circular" alt={user.result.name}>
-                                { user.result.name.charAt(0) }
-                            </Avatar>
-                            <Typography variant="h6" component="h6" color="inherit" align="center" noWrap={true}>
-                                { user.result.name }
-                            </Typography>
-                            <Button className={classes.logout} variant="contained" color="primary" size="small" href="/" onClick={logout}>Logout</Button>
-                        </div>
-                    ) : (
-                            <Button variant="contained" color="primary" size="small" href="/auth">
-                                Sign In
-                            </Button>
-                        )
-                }
+                { user ? (
+                    <div className={classes.profile}>
+                        <Avatar src={user.result.imageUrl} variant="circular" alt={user.result.name}>
+                            { user.result.name.charAt(0) }
+                        </Avatar>
+                        <Typography variant="h6" component="h6" color="inherit" align="center" noWrap={true}>
+                            { user.result.name }
+                        </Typography>
+                        <Button className={classes.logout} variant="contained" color="primary" size="small" href="/" onClick={logout}>
+                            Logout
+                        </Button>
+                    </div>
+                ) : (
+                    <Button variant="contained" color="primary" size="small" href="/auth" >
+                        Sign in
+                    </Button>
+                )}
             </Toolbar>
         </AppBar>
     )
 }
 
 export default Navbar
-
-
--- server/validation/Auth/AuthValidation.js
+++ server/validation/Auth/AuthValidation.js
@@ -1,26 +1,26 @@
 import joi from 'joi'
 
 export const registrationValidation = (body) => {
-    const registerBody = {
-        firstName: joi.string().min(6).max(255).required(),
-        lastName: joi.string().min(6).max(255).required(),
-        email: joi.string().min(6).max(255).required().email(),
-        password: joi.string().min(6).max(255).required(),
-        confirmPassword: joi.string().min(6).max(255).required()
-    }
+    const schema = joi.object({
+        firstName: joi.string().min(5).max(255).required(),
+        lastName: joi.string().min(5).max(255).required(),
+        email: joi.string().min(5).max(255).required().email(),
+        password: joi.string().min(5).max(255).required(),
+        confirmPassword: joi.string().min(5).max(255).required()
+    })
 
-    return joi.object(registerBody).validate(body)
+    return schema.validate(body)
 }
 
 export const loginValidation = (body) => {
-    const loginBody = {
-        email: joi.string().min(6).max(255).required().email(),
-        password: joi.string().min(6).max(255).required()
-    }
+    const schema = joi.object({
+        email: joi.string().min(5).max(255).required().email(),
+        password: joi.string().min(5).max(255).required()
+    })
 
-    return joi.object(loginBody).validate(body)
+    return schema.validate(body)
 }
 
 export const forgetPassword = (email) => {
-    return joi.string().min(6).max(255).required().email().validate(email)
+    return joi.string().min(5).max(255).required().email().validate(email)
 }
